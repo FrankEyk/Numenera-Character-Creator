f8d80974da27a26ce7f092ffb116dcf47f3fc312
Frank <frank_eykelhoff@yahoo.de>
2021-02-04 20:28:51 +0100

    created fields for main fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Numenera-Character-Creator';
+  charName = '';
+  descriptors = ['Glaive', 'Nano', 'Jack'];
+  foci = ['Bears a Halo of Fire',
+  'Commands Mental Powers',
+  'Controls Beasts',
+  'Controls Gravity',
+  'Crafts Illusions',
+  'Employs Magnetism',
+  'Entertains',
+  'Exists Partially Out of Phase',
+  'Explores Dark Places',
+  'Fights With Panache',
+  'Focuses Mind Over Matter',
+  'Fuses Flesh and Steel',
+  'Howls at the Moon',
+  'Hunts',
+  'Lives in the Wilderness',
+  'Masters Defense',
+  'Masters Weaponry',
+  'Murders',
+  'Rages',
+  'Rides the Lightning',
+  'Speaks With a Silver Tongue',
+  'Talks to Machines',
+  'Wears a Sheen of Ice',
+  'Wields Power With Precision',
+  'Wields Two Weapons at Once',
+  'Works the Back Alleys',
+  'Works Miracles',
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input'; 
+import {MatSelectModule} from '@angular/material/select'; 
 
 @NgModule({
   declarations: [
@@ -12,7 +17,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
