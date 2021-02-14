ac5c2bdcb3dc37afcf272c3654614124ba462b51
Frank <frank_eykelhoff@yahoo.de>
2021-02-14 21:35:30 +0100

    cleanup subscriptions

--- src/app/components/info-panel/info-panel.component.ts
+++ src/app/components/info-panel/info-panel.component.ts
@@ -1,41 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TypeService } from '../../services/type.service';
 import { DescriptorService } from '../../services/descriptor.service';
 import { Benefit } from '../../model/benefit.model';
 import { FocusService } from '../../services/focus.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-info-panel',
   templateUrl: './info-panel.component.html',
   styleUrls: ['./info-panel.component.css']
 })
-export class InfoPanelComponent implements OnInit {
+export class InfoPanelComponent implements OnInit, OnDestroy {
   selectedName = '';
   selectedDesc = '';
   benefits: Benefit[] = [];
+
+  private subs: Subscription[] = [];
 
   constructor(private typeService: TypeService,
     private descService: DescriptorService,
     private focusService: FocusService) { }
 
+  ngOnDestroy(): void {
+    this.subs.forEach(subscription => subscription.unsubscribe());
+  }
+
   ngOnInit(): void {
-    this.typeService.subToSelected().subscribe(type => {
+    this.subs.push(this.typeService.subToSelected().subscribe(type => {
       this.selectedName = type.name;
       this.selectedDesc = type.description;
       this.benefits = [];
-    });
+    }));
 
-    this.descService.subToSelected().subscribe(desc => {
+    this.subs.push(this.descService.subToSelected().subscribe(desc => {
       this.selectedName = desc.name;
       this.selectedDesc = desc.description;
       this.benefits = desc.benefits;
-    });
+    }));
 
-    this.focusService.subToSelected().subscribe(focus => {
+    this.subs.push(this.focusService.subToSelected().subscribe(focus => {
       this.selectedName = focus.name;
       this.selectedDesc = focus.description;
       this.benefits = [];
-    });
+    }));
   }
 
 }
